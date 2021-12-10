c40ce4d1e676f614d76e1bb56e762beb1b65e358
Brendon-Nogueira <brendonnogueira21@gmail.com>
2021-12-09 21:20:54 -0300

    update

--- js/index.js
+++ js/index.js
@@ -2,15 +2,20 @@ function start(){
 
     const user = document.getElementById('user');
     const pass = document.getElementById('pass');
-    const btn = document.getElementById('btn');
-
-       btn =  btn.addEventListener('submit', () => {
-        if(user.value == 'admin' && pass.value == 'admin123'){
-            window.prompt('Bem Vindos');
-        }else{
-            window.prompt('Confira seu usuário e senha');
-        }
-    }
-, false);}
+   const btn = document.getElementById('btn');
+    btn.onclick = login()
 
 start();
+
+
+
+ const login =  btn.addEventListener('btn', () => {
+
+    const buttom = document.getElementById('btn');
+    if(user.value == 'admin' && pass.value == 'admin123'){
+        window.prompt('Bem Vindos');
+    }else{
+        window.prompt('Confira seu usuário e senha');
+    }
+}
+, false);}
