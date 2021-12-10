cd4ff86da18f7be6afe2aa4b2f289d609efec221
Brendon-Nogueira <brendonnogueira21@gmail.com>
2021-12-09 22:33:40 -0300

    update

--- js/index.js
+++ js/index.js
@@ -1,26 +1,22 @@
-function start(){
+function start() {
 
-   const btn = document.getElementById('btn').value
-   newFunction(btn)
-}
-    const loginEvent = _Event => {
-
-        const user = document.getElementById('user').value
-        const pass = document.getElementById('pass').value
-
-        if (user === 'admin' && pass === 'admin123') {
-            Window.prompt('Welcome, have a great day')
-        } else {
-            Window.prompt('Invalid')
-        }
-
-
+    const btn = document.getElementById('btn')
+    btn.onclick = loginCheck
+  }
+  
+  function loginCheck() { 
+  
+    const user = document.getElementById('user')
+    const password = document.getElementById('password')
+  
+      if ( user.value == 'admin' && password.value == 'admin123') {
+        window.prompt('Welcome, have a nice day!')
+  
+      }
+      else  {
+        window.prompt('Wrong username or password, please try again')
+      }
+  
     }
-
-start();
-
-
-function newFunction(btn) {
-    btn.onclick = loginEvent
-}
-
+  
+  start()
